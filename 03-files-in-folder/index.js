54270a674aa51a1dd46bae2de1081ea32f5d6320
Anastasiia Oshurkova <adoshurkova@gmail.com>
2024-01-05 00:10:14 +0300

    fix: 03-files-in-folder lint

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -5,26 +5,33 @@ let aboutFiles = function (file) {
   let data = [];
 
   if (file.isFile()) {
-    fs.stat(path.resolve(__dirname, 'secret-folder', file.name), function (error, stats) {
-      if (error) {
-        return console.log(error);
-      }
+    fs.stat(
+      path.resolve(__dirname, 'secret-folder', file.name),
+      function (error, stats) {
+        if (error) {
+          return console.log(error);
+        }
 
-      data.push(file.name.split('.').slice(0, -1).join('.'));
-      data.push(path.extname(file.name).slice(1));
-      data.push((Math.round(stats.size/1024)) + 'Kb');
+        data.push(file.name.split('.').slice(0, -1).join('.'));
+        data.push(path.extname(file.name).slice(1));
+        data.push(Math.round(stats.size / 1024) + 'Kb');
 
-      console.log(data.join(' - '));
-    });
+        console.log(data.join(' - '));
+      },
+    );
   }
 };
 
-fs.readdir(path.join(__dirname, 'secret-folder'), { withFileTypes: true }, function (error, files) {
-  if (error) {
-    return console.log(error);
-  }
+fs.readdir(
+  path.join(__dirname, 'secret-folder'),
+  { withFileTypes: true },
+  function (error, files) {
+    if (error) {
+      return console.log(error);
+    }
 
-  files.forEach(item => {
-    aboutFiles(item);
-  });
-});
+    files.forEach((item) => {
+      aboutFiles(item);
+    });
+  },
+);
